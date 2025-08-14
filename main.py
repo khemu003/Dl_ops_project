a319cb24f1eca7112a768eb99ad04c1045a413ee
Khemraj Mangal <mangalkhemraj@gmail.com>
2025-08-14 17:26:22 +0530

    added

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 from src.CnnClassifer import logger
+from src.CnnClassifer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.CnnClassifer.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
-logger.info("Welcome to the CNN Classifier module.")
+logger.info("\n\n=================================================")
+
+logger.info(">>>>>> Welcome to my personal Log <<<<<<")
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare Base Model" 
+try: 
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<") 
+    prepare_base_model = PrepareBaseModelTrainingPipeline() 
+    prepare_base_model.main() 
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<\n") 
+except Exception as e:
+    logger.exception(e)
+    raise e
